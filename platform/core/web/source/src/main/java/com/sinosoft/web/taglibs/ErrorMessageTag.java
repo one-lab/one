cd0e18a5e714e576d3f19fe9e6ba2d0f9c82ecc4
ymg <ymg@gmail.com>
2012-07-12 22:17:29 +0800

    添加Validation和相关的Annotation以及标签

---
+++ platform/core/web/source/src/main/java/com/sinosoft/web/taglibs/ErrorMessageTag.java
@@ -0,0 +1,85 @@
+package com.sinosoft.web.taglibs;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+import javax.validation.ConstraintViolation;
+import javax.validation.constraints.Max;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.paoding.rose.web.Invocation;
+import net.paoding.rose.web.InvocationUtils;
+/**
+ * jsp页面的<code><errorMessage /></code>标签
+ * @author Morgan
+ *
+ */
+public class ErrorMessageTag extends TagSupport{
+
+	private static final long serialVersionUID = 8479034058653691234L;
+	
+	private static Log logger = LogFactory.getLog(ErrorMessageTag.class);
+
+	private String property  ;
+	
+	private String type ;
+	
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		Invocation invocation = InvocationUtils.getCurrentThreadInvocation();
+		
+		if(invocation != null){
+			@SuppressWarnings("unchecked")
+			ConstraintViolation<Object> cv = (ConstraintViolation<Object>) 
+					invocation.getModel(this.property+"ErrorMsg");
+			if(cv != null){
+				JspWriter out = pageContext.getOut();
+				
+				if (logger.isDebugEnabled()) {
+	                logger.debug("getErrorMessage: " + property + "=" + cv);
+	            }
+				try {
+					if(type == null){
+						out.print("错误的值:" + cv.getInvalidValue() + "  " + cv.getMessage());
+					} else if(type.equalsIgnoreCase("propertyPath")){
+						out.print(cv.getPropertyPath());
+					} else if(type.equalsIgnoreCase("message")){
+						out.print(cv.getMessage());
+					} else if(type.equalsIgnoreCase("invalidValue")){
+						out.print(cv.getInvalidValue());
+					}
+				} catch (IOException e) {
+					logger.error(e);
+				}
+				
+			}
+		}
+		return super.doStartTag();
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ platform/core/web/source/src/main/java/com/sinosoft/web/validation/annotation/AssertTrueEx.java
@@ -0,0 +1,31 @@
+
+package com.sinosoft.web.validation.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.PARAMETER;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+/**
+ * The annotated element must be true.
+ * Supported types are <code>boolean</code> and <code>Boolean</code>
+ * <p/>
+ * <code>null</code> elements are considered valid.
+ *
+ * @author Morgan
+ */
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+public @interface AssertTrueEx {
+	String message() default "{javax.validation.constraints.AssertTrue.message}";
+
+	String[] props() ;
+
+
+}
