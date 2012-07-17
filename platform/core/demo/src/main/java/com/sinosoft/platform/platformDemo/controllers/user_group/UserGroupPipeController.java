e59807e8d8e3db2ba569c9af851f72b731a7cc1b
Cheng Qi <q.sanctity@gmail.com>
2012-07-17 18:23:34 +0800

    no message

--- platform/core/demo/src/main/java/com/sinosoft/platform/platformDemo/controllers/user_group/UserGroupPipeController.java
+++ platform/core/demo/src/main/java/com/sinosoft/platform/platformDemo/controllers/user_group/UserGroupPipeController.java
@@ -4,18 +4,21 @@ package com.sinosoft.platform.platformDemo.controllers.user_group;
 import net.paoding.rose.web.annotation.Path;
 import net.paoding.rose.web.annotation.rest.Get;
 import net.paoding.rose.web.portal.Pipe;
+import net.paoding.rose.web.portal.Window;
 
 @Path("pipe")
 public class UserGroupPipeController {
 
 	@Get("/main")
 	public String portalViews(Pipe p){
-//		p.addWindow("p1", "/account/user/p1");
-//		p.addWindow("p2", "/account/group/p2");
-		
-		p.addWindow("p1", "/user_group/pipe/p1");
-		p.addWindow("p2", "/user_group/pipe/p2");
-		return "user_group_pipe";
+		Window window = p.addWindow("p1", "/account/user/p1");
+        Window window2 =p.addWindow("p2", "/account/group/p2");
+
+//		p.addWindow("p1", "/user_group/pipe/p1");
+//		p.addWindow("p2", "/user_group/pipe/p2");
+//        if(window.isDone()&&window2.isDone())
+		    return "user_group_pipe";
+//        return null;
 	}
 	
 	@Get("p1")
