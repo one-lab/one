e7af2be38fe19abb5f191fdc072bfe2632bcb707
ymg <ymg@gmail.com>
2012-07-20 17:08:00 +0800

    修改冲突

--- platform/core/web/source/src/main/java/com/sinosoft/one/mvc/web/validation/AnnotationConfigValidator.java
+++ platform/core/web/source/src/main/java/com/sinosoft/one/mvc/web/validation/AnnotationConfigValidator.java
@@ -127,9 +127,7 @@ public class AnnotationConfigValidator implements ParamValidator{
 			config.addMapping( getMapping(configValidation,metaData));
 			return  config.buildValidatorFactory().getValidator();
 		}
-		
 		return defaultValidator;
-		
 	}
 	
 	
