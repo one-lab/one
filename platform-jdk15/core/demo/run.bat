5b2ed7108ab7acf9f660b9d03a4565cabc0a1577
huangyangjun <huangyangjun@sinosoft.com.cn>
2012-08-01 13:29:06 +0800

    delete project platform-jdk15

--- platform-jdk15/core/demo/run.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0
-set MAVEN_BATCH_PAUSE=on
-set MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,suspend=n,server=y -Xms256m -Xmx512m -XX:MaxPermSize=128m
-mvn tomcat:run
--- platform-jdk15/core/web/source/src/main/java/com/sinosoft/one/mvc/web/impl/mapping/ignored/IgnoredPath.java
+++
@@ -1,8 +0,0 @@
-package com.sinosoft.one.mvc.web.impl.mapping.ignored;
-
-import com.sinosoft.one.mvc.web.RequestPath;
-
-public interface IgnoredPath {
-
-    public boolean hit(RequestPath requestPath);
-}
