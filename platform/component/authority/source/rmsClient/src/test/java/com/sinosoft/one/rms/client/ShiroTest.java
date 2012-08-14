282a690f48330e1412c6c4ef59db443cb69c92f4
chenxiongxi <chenxiongxi@sinosoft.com.cn>
2012-08-14 16:00:11 +0800

    shirotest

---
+++ platform/component/authority/source/rmsClient/src/test/java/com/sinosoft/one/rms/client/ShiroTest.java
@@ -0,0 +1,25 @@
+package com.sinosoft.one.rms.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@DirtiesContext
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml","/spring/applicationContext-shiro.xml",
+		"/spring/applicationContext-rms.xml"
+})
+public class ShiroTest {
+	@Autowired
+	private AccountManager accountManager;
+	
+	@Test
+	public void testShiroDbRealm(){
+		accountManager.findUserByLoginName("admin", "00");
+	}
+	
+}
