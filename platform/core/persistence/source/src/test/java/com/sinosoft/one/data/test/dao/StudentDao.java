1596fb14847f85c09ed5eed86f70424a4670cf31
hanchunliang <hancl@ufida.com>
2012-08-17 19:55:15 +0800

    update

--- platform/core/persistence/source/src/test/java/com/sinosoft/one/data/test/dao/StudentDao.java
+++ platform/core/persistence/source/src/test/java/com/sinosoft/one/data/test/dao/StudentDao.java
@@ -3,6 +3,8 @@ package com.sinosoft.one.data.test.dao;
 import com.sinosoft.one.data.test.model.Student;
 import com.sinosoft.one.data.test.model.User;
 import com.sinosoft.one.data.jade.annotation.SQL;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -37,7 +39,7 @@ public interface StudentDao extends CrudRepository<Student, String> {
 	List<User> sqlQuery2(String id);
 	@Transactional(readOnly = true)
 	List<User> sqlQuery3(@Param("id") String id);
-	@SQL("select * from t_student")
+	@SQL("select * from t_student where id=:user.id")
 	@Transactional(readOnly = true)
 	List<User> sqlQuery4(@Param("user") User user,@Param("user2") User user2);
 	List<User> getSSS();
