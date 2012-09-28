3614fedc623841fe3b362f65fd3062a5d1b3f5e6
chenxiongxi <chenxiongxi@sinosoft.com.cn>
2012-09-28 15:56:03 +0800

    test

--- platform/component/authority/source/rms/src/test/java/com/sinosoft/one/service/spring/RmsServiceSpringImplTest.java
+++ platform/component/authority/source/rms/src/test/java/com/sinosoft/one/service/spring/RmsServiceSpringImplTest.java
@@ -4,20 +4,17 @@ package com.sinosoft.one.service.spring;
 import java.util.ArrayList;
 import java.util.List;
 
-import oracle.net.aso.r;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import com.sinosoft.one.rms.model.Company;
+import com.sinosoft.one.rms.service.facade.CompanyServiceInterface;
 import com.sinosoft.one.rms.service.facade.RmsService;
-import com.sinosoft.one.rms.service.spring.RmsServiceSpringImpl;
 
 @DirtiesContext
 @ContextConfiguration(locations = { "/spring/applicationContext-rmstest.xml",
@@ -25,11 +22,13 @@ import com.sinosoft.one.rms.service.spring.RmsServiceSpringImpl;
 @TransactionConfiguration(transactionManager = "defaultTransactionManager",defaultRollback=false)
 public class RmsServiceSpringImplTest extends AbstractJUnit4SpringContextTests {
 	@Autowired
+	private CompanyServiceInterface companyServiceInterface;
+	@Autowired
 	private RmsService rmsService;
 	@Test
 	public void testAllNextLevelCompanyByComCode(){
 		List<Company> companies=new ArrayList<Company>();
-		companies=rmsService.findAllNextLevelCompanyByComCode("11");
+		companies=(List<Company>) companyServiceInterface.findAllNextLevelCompanybyComCode("11");
 		companies.get(0).getComCName();
 		System.out.println(companies.get(0).getComCName());
  		Assert.assertEquals(companies.get(0).getComCName(),"北京分公司");
@@ -37,6 +36,7 @@ public class RmsServiceSpringImplTest extends AbstractJUnit4SpringContextTests {
 	
 	@Test
 	public void testfindCompanyBySuperAndType(){
-		Assert.assertEquals(rmsService.findCompanyBySuperAndType("00", "1").size(),25);
+		Assert.assertEquals(companyServiceInterface.findCompanyBySuperAndType("00", "1").size(),25);
 	}
+	
 }
