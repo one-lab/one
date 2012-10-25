50824bc18f8900921399fb7918c26e89b04140ab
kylin <kylin@kylin.com>
2012-10-25 16:05:38 +0800

    nothing

---
+++ platform/core/demo/src/main/java/com/sinosoft/one/demo/service/timer/QuartzService.java
@@ -0,0 +1,23 @@
+package com.sinosoft.one.demo.service.timer;
+
+import com.sinosoft.one.demo.dao.account.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Intro:
+ * User: Kylin
+ * Date: 12-10-24
+ * Time: 上午11:22
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class QuartzService {
+//    @Autowired
+//    private UserDao userDao;
+    public void invoke() {
+        System.out.println("quartz service invoke method begin...............");
+//        userDao.findAll();
+        System.out.println("quartz service invoke method end...............");
+    }
+}
