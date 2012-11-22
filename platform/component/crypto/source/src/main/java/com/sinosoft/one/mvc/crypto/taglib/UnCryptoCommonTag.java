079e88c5367ec2c6db511d9257205691bf8a5a11
zfb <zfb>
2012-11-22 09:58:35 +0800

    增加普通属性的前台解密（支持多td等）

--- platform/component/crypto/source/src/main/java/com/sinosoft/one/mvc/crypto/taglib/UnCryptoCommonTag.java
+++ platform/component/crypto/source/src/main/java/com/sinosoft/one/mvc/crypto/taglib/UnCryptoCommonTag.java
@@ -1,6 +1,7 @@
 package com.sinosoft.one.mvc.crypto.taglib;
 
 import com.sinosoft.one.mvc.crypto.config.CryptoConfig;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -16,45 +17,66 @@ import java.io.IOException;
  * Time: 上午11:16
  */
 public class UnCryptoCommonTag extends TagSupport {
-	private static Log logger = LogFactory.getLog(UnCryptoCommonTag.class);
-	private String eType;
-	private String eIds;
+    private static Log logger = LogFactory.getLog(UnCryptoCommonTag.class);
+    private String eType;
+    private String eIds;
+    private String eClasses;
 
-	public String geteType() {
-		return eType;
-	}
+    public String geteType() {
+        return eType;
+    }
 
-	public void seteType(String eType) {
-		this.eType = eType;
-	}
+    public void seteType(String eType) {
+        this.eType = eType;
+    }
 
-	public String geteIds() {
-		return eIds;
-	}
+    public String geteIds() {
+        return eIds;
+    }
 
-	public void seteIds(String eIds) {
-		this.eIds = eIds;
-	}
+    public void seteIds(String eIds) {
+        this.eIds = eIds;
+    }
 
-	@Override
-	public int doStartTag() throws JspException {
+    public String geteClasses() {
+        return eClasses;
+    }
 
-		String key = (String) ((HttpServletRequest)pageContext.getRequest()).getSession()
-				.getAttribute(CryptoConfig.CRYPTO_KEY_ATTR_NAME);
-		try {
-			JspWriter out = pageContext.getOut();
-			out.append("<script type=\"text/javascript\">( function(){");
-			String[] ids = eIds.split(",");
-			for( int l=ids.length,i=0;i<l;i++ ){
-				out.append("$$crypdata=$(\"#").append(ids[i])
-						.append("\").text();$(\"#").append(ids[i])
-						.append("\").text(decode('"+key+"',$$crypdata));");
-			}
-			out.append("} )();</script>");
-			out.flush();
-		} catch (IOException e) {
-			logger.error(e);
-		}
-		return super.doStartTag();
-	}
+    public void seteClasses(String eClasses) {
+        this.eClasses = eClasses;
+    }
+
+    @Override
+    public int doStartTag() throws JspException {
+
+        String key = (String) ((HttpServletRequest) pageContext.getRequest()).getSession()
+                .getAttribute(CryptoConfig.CRYPTO_KEY_ATTR_NAME);
+        try {
+            JspWriter out = pageContext.getOut();
+            if (!StringUtils.isBlank(eIds)) {
+                out.append("<script type=\"text/javascript\">( function(){");
+                String[] ids = eIds.split(",");
+                for (int l = ids.length, i = 0; i < l; i++) {
+                    out.append("$$crypdata=$(\"#").append(ids[i])
+                            .append("\").text();$(\"#").append(ids[i])
+                            .append("\").text(decode('" + key + "',$$crypdata));");
+                }
+                out.append("} )();</script>");
+            }
+            if (!StringUtils.isBlank(eClasses)) {
+                out.append("<script type=\"text/javascript\">( function(){");
+                String[] classes = eClasses.split(",");
+                for (int l = classes.length, i = 0; i < l; i++) {
+                    out.append("$$crypdata=$(\".").append(classes[i])
+                            .append("\").text();$(\".").append(classes[i])
+                            .append("\").text(decode('" + key + "',$$crypdata));");
+                }
+                out.append("} )();</script>");
+            }
+            out.flush();
+        } catch (IOException e) {
+            logger.error(e);
+        }
+        return super.doStartTag();
+    }
 }
