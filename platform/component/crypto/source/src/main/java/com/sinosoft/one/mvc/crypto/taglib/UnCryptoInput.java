fb421882125ffcb21b740ee81ae37fe26f708110
zfb <zfb>
2012-12-04 15:55:46 +0800

    1204-<input>标签前台解密bug的修正

--- platform/component/crypto/source/src/main/java/com/sinosoft/one/mvc/crypto/taglib/UnCryptoInput.java
+++ platform/component/crypto/source/src/main/java/com/sinosoft/one/mvc/crypto/taglib/UnCryptoInput.java
@@ -1,6 +1,7 @@
 package com.sinosoft.one.mvc.crypto.taglib;
 
 import com.sinosoft.one.mvc.crypto.config.CryptoConfig;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -16,27 +17,38 @@ import java.io.IOException;
  * Time: 下午7:41
  */
 public class UnCryptoInput extends TagSupport {
-	private static Log logger = LogFactory.getLog(UnCryptoInput.class);
+    private static Log logger = LogFactory.getLog(UnCryptoInput.class);
+    private String formIds;
 
-	public int doStartTag() throws JspException {
+    public String getFormIds() {
+        return formIds;
+    }
 
-		JspWriter out = pageContext.getOut();
-		String names = (String) pageContext.getRequest().getAttribute(CryptoConfig.UNCRYPTO_ATTR_NAMES);
-		String key = (String) ((HttpServletRequest)pageContext.getRequest()).getSession()
-				.getAttribute(CryptoConfig.CRYPTO_KEY_ATTR_NAME);
-		try {
-			out.append("<script type=\"text/javascript\">( function(){");
-			String[] ids = names.split(",");
-			for( int l=ids.length,i=0;i<l;i++ ){
-				out.append("$$crypdata=$(\"input[name='").append(ids[i])
-						.append("']\").val();$(\"input[name='").append(ids[i])
-						.append("']\").val(decode('"+key+"',$$crypdata));");
-			}
-			out.append("} )();</script>");
-			out.flush();
-		} catch (IOException e) {
-			logger.error(e);
-		}
-		return super.doStartTag();
-	}
+    public void setFormIds(String formIds) {
+        this.formIds = formIds;
+    }
+
+    public int doStartTag() throws JspException {
+
+        JspWriter out = pageContext.getOut();
+        String key = (String) ((HttpServletRequest) pageContext.getRequest()).getSession()
+                .getAttribute(CryptoConfig.CRYPTO_KEY_ATTR_NAME);
+        if (!StringUtils.isBlank(formIds)) {
+            try {
+                out.append("<script type=\"text/javascript\">( function(){");
+                String[] eFormIds = formIds.split(",");
+                for (String eFormId : eFormIds) {
+                    out.append("var i=0;var j=$(\"#").append(eFormId).append("\").find(\"input\").length;for(i=0;i<j;i++){")
+                            .append("$$crypdata=$(\"#").append(eFormId).append("\").find(\"input\").eq(i).val();")
+                            .append("$(\"#").append(eFormId).append("\").find(\"input\").eq(i).val(decode('")
+                            .append(key).append("',$$crypdata));}");
+                }
+                out.append("})();</script>");
+                out.flush();
+            } catch (IOException e) {
+                logger.error(e);
+            }
+        }
+        return super.doStartTag();
+    }
 }
