0b941d40f4c401a54c1bdd916e53c621633eff0c
chenxiongxi <chenxiongxi@sinosoft.com.cn>
2012-12-12 17:28:08 +0800

    dao

--- platform/core/ams/src/main/java/com/sinosoft/one/ams/repositories/GeRmsTaskAuthRepository.java
+++ platform/core/ams/src/main/java/com/sinosoft/one/ams/repositories/GeRmsTaskAuthRepository.java
@@ -9,8 +9,6 @@ import com.sinosoft.one.data.jade.annotation.SQL;
 
 public interface GeRmsTaskAuthRepository extends PagingAndSortingRepository<TaskAuth, String>{
 	
-	@SQL("insert into GE_RMS_TASK_AUTH values(?1,?2,?3,?4,'e')")
-	void InsertTask(String taskAuthID,String taskID,String comCode, String operateUser);
 	
 	@SQL("delete from GE_RMS_TASK_AUTH where comcode=?1 and taskid=?2")
 	void deleteTask(String comCode,String taskId);
@@ -20,12 +18,12 @@ public interface GeRmsTaskAuthRepository extends PagingAndSortingRepository<Task
 	void updateTaskAuth(String flag,String comCode,String taskId); 
 	
 	//根据机构ID查询出功能ID
-	@SQL("select taskId from GE_RMS_TASK_AUTH where comcode=?1")
+	@SQL("select taskId from GE_RMS_TASK_AUTH where comcode=?1 or comCode='*'")
 	List<String> findTaskIdByComCode(String comCode);
 	
 	//根据角色id查询角色关联的授权
-	@SQL("select * from GE_RMS_TASK_AUTH where taskAuthID in(select taskAuthID from ge_rms_roletask where roleid=?1)")
-	List<TaskAuth> findTaskAuthByRole(String roleId);
+	@SQL("select taskid from GE_RMS_TASK_AUTH where taskAuthID in(select taskAuthID from ge_rms_roletask where roleid=?1)")
+	List<String> findTaskAuthByRole(String roleId);
 	
 	//查询已授权可用功能
 	@SQL("select * from GE_RMS_TASK_AUTH where comCode in ('*',?1) and taskID in (?2)")
