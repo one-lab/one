5c466381ea99d43e1fecf658f24ae68eac0ecfae
chenxiongxi <chenxiongxi@sinosoft.com.cn>
2012-12-13 12:17:22 +0800

    RoleDesignateReposity

---
+++ platform/core/ams/src/main/java/com/sinosoft/one/ams/repositories/GeRmsRoleDesignateRepository.java
@@ -0,0 +1,10 @@
+package com.sinosoft.one.ams.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.sinosoft.one.ams.model.Group;
+import com.sinosoft.one.ams.model.RoleDesignate;
+
+public interface GeRmsRoleDesignateRepository  extends PagingAndSortingRepository<RoleDesignate, String>{
+
+}
