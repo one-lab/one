b953299e9c64e6dfc11283b15ed3341bb5e1002f
Cheng Qi <q.sanctity@gmail.com>
2013-03-05 17:26:12 +0800

    可用性

--- platform/product/monitor/src/main/java/com/sinosoft/one/monitor/application/repository/EumUrlAvaStaRepository.java
+++ platform/product/monitor/src/main/java/com/sinosoft/one/monitor/application/repository/EumUrlAvaStaRepository.java
@@ -1,10 +1,16 @@
 package com.sinosoft.one.monitor.application.repository;
 // Generated 2013-3-4 15:45:31 by One Data Tools 1.0.0
 
+import com.sinosoft.one.monitor.application.model.EumUrl;
+import com.sinosoft.one.monitor.application.model.EumUrlAvaSta;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.sinosoft.one.monitor.EumUrlAvaSta;
+import java.util.Date;
+import java.util.List;
+
 
 public interface EumUrlAvaStaRepository extends PagingAndSortingRepository<EumUrlAvaSta, String> {
+
+    List<EumUrlAvaSta> findByRecordtimeAndEumUrl(Date Date,EumUrl eumUrl);
 }
 
--- platform/product/monitor/src/main/java/com/sinosoft/one/monitor/application/repository/EumUrlRepository.java
+++ platform/product/monitor/src/main/java/com/sinosoft/one/monitor/application/repository/EumUrlRepository.java
@@ -1,9 +1,9 @@
 package com.sinosoft.one.monitor.application.repository;
 // Generated 2013-3-4 15:45:31 by One Data Tools 1.0.0
 
+import com.sinosoft.one.monitor.application.model.EumUrl;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.sinosoft.one.monitor.EumUrl;
 
 public interface EumUrlRepository extends PagingAndSortingRepository<EumUrl, String> {
 }
