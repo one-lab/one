a1a78635a2704aba905893886bbb6c703f78e91e
Cheng Qi <q.sanctity@gmail.com>
2013-03-05 16:49:19 +0800

    业务仿真

---
+++ platform/product/monitor/src/test/java/com/sinosoft/one/monitor/application/EumUrlAvaRepoTest.java
@@ -0,0 +1,11 @@
+package com.sinosoft.one.monitor.application;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ChengQi
+ * Date: 13-3-5
+ * Time: PM4:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class EumUrlAvaRepoTest {
+}
