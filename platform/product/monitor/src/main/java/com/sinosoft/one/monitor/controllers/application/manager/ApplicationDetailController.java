a338d6daa76d3d550f5431cbf3aac49ee9f5fcdb
huangyangjun <huangyangjun@sinosoft.com.cn>
2013-03-10 22:43:11 +0800

    update fastjson version and use druid datasource

--- platform/product/monitor/src/main/java/com/sinosoft/one/monitor/controllers/application/manager/ApplicationDetailController.java
+++ platform/product/monitor/src/main/java/com/sinosoft/one/monitor/controllers/application/manager/ApplicationDetailController.java
@@ -3,6 +3,7 @@ package com.sinosoft.one.monitor.controllers.application.manager;
 import com.sinosoft.one.monitor.application.domain.ApplicationAvailableInf;
 import com.sinosoft.one.monitor.application.domain.ApplicationDetailService;
 import com.sinosoft.one.monitor.application.domain.ApplicationEmuService;
+import com.sinosoft.one.monitor.application.domain.EumUrlsNotFoundException;
 import com.sinosoft.one.monitor.application.model.BizScenario;
 import com.sinosoft.one.monitor.application.model.UrlResponseTime;
 import com.sinosoft.one.monitor.application.model.viewmodel.ApplicationDetailAlarmViewModel;
@@ -42,7 +43,12 @@ public class ApplicationDetailController {
 	@Get("/main/{applicationId}")
 	public String applicationDetail(@Param("applicationId") String applicationId, Portal portal, Invocation invocation) {
 		invocation.addModel("applicationId", applicationId);
-		ApplicationAvailableInf applicationAvailableInf = applicationEmuService.getApplicationAvailableToday(applicationId);
+		ApplicationAvailableInf applicationAvailableInf = null;
+		try {
+			applicationAvailableInf = applicationEmuService.getApplicationAvailableToday(applicationId);
+		} catch (EumUrlsNotFoundException e) {
+
+		}
 		invocation.addModel("applicationAvailableInf", applicationAvailableInf);
 		portal.addWindow("alarm", "/application/manager/detail/alarm/" + applicationId);
 		portal.addWindow("pie", "/application/manager/detail/pie/" + applicationId);
@@ -53,11 +59,13 @@ public class ApplicationDetailController {
 	public String alarm(@Param("applicationId") String applicationId, Invocation invocation) {
 		ApplicationAvailableInf applicationAvailableInf = (ApplicationAvailableInf)invocation.getModel("applicationAvailableInf");
 		ApplicationDetailAlarmViewModel applicationDetailAlarmViewModel = applicationDetailService.generateAlarmViewModel(applicationId);
-		String availability = "";
-		if(applicationAvailableInf.getTrend() == Trend.DROP) {
-			availability = "idown";
-		} else if(applicationAvailableInf.getTrend() == Trend.RISE) {
-			availability = "iup";
+		String availability = "iup";
+		if(applicationAvailableInf != null) {
+			if(applicationAvailableInf.getTrend() == Trend.DROP) {
+				availability = "idown";
+			} else if(applicationAvailableInf.getTrend() == Trend.RISE) {
+				availability = "iup";
+			}
 		}
 		applicationDetailAlarmViewModel.setAvailability(availability);
 		invocation.addModel("alarmViewModel", applicationDetailAlarmViewModel);
@@ -66,10 +74,17 @@ public class ApplicationDetailController {
 
 	@Get("/pie/{applicationId}")
 	public String pie(@Param("applicationId") String applicationId, Invocation invocation) {
-		ApplicationAvailableInf applicationAvailableInf = (ApplicationAvailableInf)invocation.getModel("applicationAvailableInf");
 		ApplicationDetailPieViewModel applicationDetailPieViewModel = applicationDetailService.generatePieViewModel(applicationId);
-		applicationDetailPieViewModel.setAvailabilityCount(applicationAvailableInf.getAvailableCount());
-		applicationDetailPieViewModel.setUnavailabilityCount(applicationAvailableInf.getCount() - applicationAvailableInf.getAvailableCount());
+		ApplicationAvailableInf applicationAvailableInf = (ApplicationAvailableInf)invocation.getModel("applicationAvailableInf");
+
+		if(applicationAvailableInf == null) {
+			applicationDetailPieViewModel.setAvailabilityCount(0);
+			applicationDetailPieViewModel.setUnavailabilityCount(0);
+		} else {
+			applicationDetailPieViewModel.setAvailabilityCount(applicationAvailableInf.getAvailableCount());
+			applicationDetailPieViewModel.setUnavailabilityCount(applicationAvailableInf.getCount() - applicationAvailableInf.getAvailableCount());
+		}
+
 		invocation.addModel("pieViewModel", applicationDetailPieViewModel);
 		return "applicationDetailPie";
 	}
